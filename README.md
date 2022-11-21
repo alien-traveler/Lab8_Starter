a81e0658f3486cdb56f3b8a40cc941f8a8c05235
Will <jiwenwillluo@gmail.com>
2022-11-20 21:42:57 -0800

    finish v1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Lab 8 - Starter
+
+# Work Alone
+
+1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+    > Within a Github action that runs whenever code is pushed. Why? Since we are practicing Agile methodology, we are updating our software with small batches of updates. Therefore, it is necessary to test these small updates in time. Github action can automatically run when we push to the main branch, so it guarantes necessary testing and is perfect for us to use. 
+2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+    > No, because we are testing user actions, so we do not know if the function returns the correct output.
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+    > No because a unit test is to test individual functions of our application. The “message” feature requires functions to interact with each other within that application, so it is not the job of a unit test. 
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+    > Yes because a unit test is to test the feature of a single function. "max message length" is a single function (feature) of this app, so we can use a unit test. 
+
+![](screenshot1.png)
+![](screenshot2.png)
+![](screenshot3.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,73 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// isPhoneNumber
+test('Test isPhoneNumber() 1', () => {
+    expect(functions.isPhoneNumber('(123)-456-7890')).toBe(true);
+});
+test('Test isPhoneNumber() 2', () => {
+    expect(functions.isPhoneNumber('999-999-9999')).toBe(true);
+});
+test('Test isPhoneNumber() 3', () => {
+    expect(functions.isPhoneNumber('123123123123123')).toBe(false);
+});
+test('Test isPhoneNumber() 4', () => {
+    expect(functions.isPhoneNumber('10101')).toBe(false);
+});
+
+// isEmail
+test('Test isEmail() 1', () => {
+    expect(functions.isEmail('123456@ucsd.edu')).toBe(true);
+});
+test('Test isEmail() 2', () => {
+    expect(functions.isEmail('luojiwen@gmail.com')).toBe(true);
+});
+test('Test isEmail() 3', () => {
+    expect(functions.isEmail('123456')).toBe(false);
+});
+test('Test isEmail() 4', () => {
+    expect(functions.isEmail('@gmail.com')).toBe(false);
+});
+
+// isStrongPassword
+test('Test isStrongPassword() 1', () => {
+    expect(functions.isStrongPassword('I103lwaei058_')).toBe(true);
+});
+test('Test isStrongPassword() 2', () => {
+    expect(functions.isStrongPassword('a1030pqiPDEi')).toBe(true);
+});
+test('Test isStrongPassword() 3', () => {
+    expect(functions.isStrongPassword('3iqwer320_')).toBe(false);
+});
+test('Test isStrongPassword() 4', () => {
+    expect(functions.isStrongPassword('WLLPQ1234567890987654321')).toBe(false);
+});
+
+// isDate
+test('Test isDate() 1', () => {
+    expect(functions.isDate('01/01/2000')).toBe(true);
+});
+test('Test isDate() 2', () => {
+    expect(functions.isDate('3/2/2004')).toBe(true);
+});
+test('Test isDate() 3', () => {
+    expect(functions.isDate('02/931/2022')).toBe(false);
+});
+test('Test isDate() 4', () => {
+    expect(functions.isDate('11/3/77')).toBe(false);
+});
+
+// isHexColor
+test('Test isHexColor() 1', () => {
+    expect(functions.isHexColor('#32b9f5')).toBe(true);
+});
+test('Test isHexColor() 2', () => {
+    expect(functions.isHexColor('#FC0')).toBe(true);
+});
+test('Test isHexColor() 3', () => {
+    expect(functions.isHexColor('#zzzzzz')).toBe(false);
+});
+test('Test isHexColor() 4', () => {
+    expect(functions.isHexColor('#d9b5')).toBe(false);
+});
